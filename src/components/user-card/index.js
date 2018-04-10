fd5d1d4bd22423efde779364f9f275af798cf984
elma.santos <elma.santos@codeminer42.com>
2018-04-10 13:22:25 -0300

    Refactor user-card content

--- src/components/user-card/index.js
+++ src/components/user-card/index.js
@@ -10,10 +10,22 @@ const UserCard = ({
   <div className='user-card'>
   <div className='user-card__name'>{name}</div>
   <div className='user-card__info'>
-    <div className='user-card__age'>Age: {age}</div>
-    <div className='user-card__job'>Job: {job}</div>
+    <div className='user-card__age'>
+      <span className='user-card__age--blue'>
+        {age}
+      </span>
+      <span>
+        &nbsp;years old
+      </span>
+    </div>
+    <div className='user-card__job'>
+      Work as&nbsp;
+      <span className='user-card__job--blue'>
+        {job}
+      </span>
+    </div>
   </div>
-
+    <div className='description'>Description</div>
     <div className='user-card__description'>{description}</div>
   </div>
 )
