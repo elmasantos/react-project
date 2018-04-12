8e1c96252bf148bbc1b6bc78b446d57accd85313
elmasantos <elmasantos94@hotmail.com>
2018-04-12 09:33:16 -0300

    Initial adjusts to change the state

--- src/components/common/auth-box.js
+++ src/components/common/auth-box.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 import '../../stylesheets/common/auth-box.css'
 
+import LoginForm from './login-form'
+
 class AuthBox extends React.Component {
   constructor(){
     super()
-    this.state = { }
+    this.state = {
+      showSignIn: false
+    }
   }
 
   renderAuthBox = ({ content }) => (
@@ -18,18 +22,34 @@ class AuthBox extends React.Component {
 
   renderButton = ({ color, title }) => (
     <button
-      onClick={() => { console.log('clicked!') }}
+      onClick={this._handleClick.bind(this)}
       className={`auth-box__button auth-box__button--${color}`}
     >
       <span className='auth-box__title'>{title}</span>
     </button>
   )
 
+  _handleClick() {
+    this.setState ({
+      showSignIn: !this.state.showSignIn
+    });
+  }
+
   render () {
+    let firstMessage;
+    let secondMessage;
+    if(this.state.showSignIn === false){
+      firstMessage = <div className='auth-box__message'>YOU ARE NOT IN.</div>
+      secondMessage = <div className='auth-box__message'>SO JUST GET IN, NOW!</div>
+    }
+    else {
+      firstMessage = <div className='auth-box__message'>Sign in:</div>
+      secondMessage = <LoginForm />
+    }
     return (
       <div className='auth-box'>
-        <div className='auth-box__message'>YOU ARE NOT IN.</div>
-        <div className='auth-box__message'>SO JUST GET IN, NOW!</div>
+        {firstMessage}
+        {secondMessage}
         <div className='auth-box__actions'>
           {this.renderButton({ color: 'red', title: 'SIGN IN'})}
           {this.renderButton({ color: 'blue', title: 'SIGN UP'})}
