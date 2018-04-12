71946ac5c57990721c3950b451964b3e9d78453b
elmasantos <elmasantos94@hotmail.com>
2018-04-12 10:58:16 -0300

    Add sign up state

--- src/components/common/auth-box.js
+++ src/components/common/auth-box.js
@@ -3,12 +3,14 @@ import React from 'react'
 import '../../stylesheets/common/auth-box.css'
 
 import LoginForm from './login-form'
+import RegistrationForm from './registration-form'
 
 class AuthBox extends React.Component {
   constructor(){
     super()
     this.state = {
-      showSignIn: false
+      showSignIn: false,
+      showSignUp: false
     }
   }
 
@@ -30,9 +32,16 @@ class AuthBox extends React.Component {
   )
 
   _handleClick(color) {
-    if (color == 'blue'){
+    if (color === 'blue'){
       this.setState ({
-        showSignIn: !this.state.showSignIn
+        showSignIn: !this.state.showSignIn,
+        showSignUp: false
+      });
+    }
+    else {
+      this.setState ({
+        showSignUp: !this.state.showSignUp,
+        showSignIn: false
       });
     }
   }
@@ -41,7 +50,7 @@ class AuthBox extends React.Component {
     let firstMessage;
     let secondMessage;
     let buttons;
-    if(this.state.showSignIn === false){
+    if(this.state.showSignIn === false && this.state.showSignUp === false){
       firstMessage = <div className='auth-box__message'>YOU ARE NOT IN.</div>
       secondMessage = <div className='auth-box__message'>SO JUST GET IN, NOW!</div>
       buttons = <div className='auth-box__actions'>
@@ -49,9 +58,13 @@ class AuthBox extends React.Component {
         {this.renderButton({ color: 'pink', title: 'SIGN UP'})}
       </div>
     }
-    else {
+    else if (this.state.showSignIn){
       firstMessage = <div className='auth-box__message'>Sign in:</div>
       secondMessage = <LoginForm />
+    }
+    else if (this.state.showSignUp){
+      firstMessage = <div className='auth-box__message'>Sign up:</div>
+      secondMessage = <RegistrationForm />
     }
     return (
       <div className='auth-box'>
