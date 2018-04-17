dd050162d44c4ae9f626eedb3e839f361727ac04
elmasantos <elmasantos94@hotmail.com>
2018-04-17 11:33:43 -0300

    Improve Search component

--- src/components/common/search.js
+++ src/components/common/search.js
@@ -1,12 +1,43 @@
 import React from 'react'
 
+import Suggestions from './suggestions'
+import usersResources from '../../services/resources/users'
+
 import '../../stylesheets/common/search.css'
 
 class Search extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      query: '',
+      results: []
+    }
+  }
+
+  getInfo = () => {
+    usersResources.getUsers()
+      .then((result) => this.setState({
+        results: result.data
+      }))
+  }
+
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    }, () => {
+      if(this.state.query && this.state.query.length > 1){
+        if(this.state.query.length % 2 === 0){
+          this.getInfo()
+        }
+      }
+    })
+  }
+
   render () {
     return (
       <div className='search'>
-        <input className='search__element' type="text" placeholder="Looking for someone?" />
+        <input className='search__element' placeholder="Looking for someone?" ref={(input) => {
+          this.search = input}} onChange={this.handleInputChange}/>
       </div>
     )
   }
